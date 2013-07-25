c7b18be316878baa0844f09e044cd31ccd1bb6ba
Vitali Makarov <master@clevertech.ru>
2013-07-25 23:40:15 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Advsurf::Application.config.secret_token = '8d50688b0c1a39f17cdcddda726fa521dc8dd23d6f168ea89a148ea8f323c1acadf5e0611915fe1ce9d5e6b0f40362431ae60cdb142d270821602d72d7d12c4c'
