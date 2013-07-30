49823e22edfb19de69d90ba0867760823af12783
Vitali Makarov <master@clevertech.ru>
2013-07-31 01:36:57 +0400

    add omniauth support

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Advsurf::Application.routes.draw do
+  match '/auth/:provider/callback' => 'authentications#create'
+  match '/users/auth/:provider/callback' => 'authentications#create'
+  match '/registrations' => 'registrations#email'
   authenticated :user do
     root :to => 'home#index'
   end
   root :to => "home#index"
   devise_for :users
   resources :users
+  resources :authentications
+
 end
